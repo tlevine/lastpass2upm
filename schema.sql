34bea88172bb35f482df9150a407c8da3f426f91
Thomas Levine <occurrence@thomaslevine.com>
2012-12-14 01:13:30 -0500

    schema

---
+++ schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE lastpass (
+  url TEXT NOT NULL,
+  username TEXT NOT NULL,
+  password TEXT NOT NULL,
+  extra TEXT NOT NULL,
+  name TEXT NOT NULL,
+  grouping TEXT NOT NULL,
+  fav TEXT NOT NULL
+);
