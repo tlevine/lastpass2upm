694b91d2d844245c7726476ddb16672b02834d3b
Thomas Levine <occurrence@thomaslevine.com>
2012-12-14 01:42:21 -0500

    move schema to lastpass2upm.sh

--- lastpass2upm.sh
+++ lastpass2upm.sh
@@ -11,7 +11,26 @@ else
   sed 1d /dev/stdin > $lastpasscsv
 fi
 
-sqlite3 $db < schema.sql
+sqlite3 $db << EOF
+CREATE TABLE lastpass (
+  url TEXT,
+  username TEXT,
+  password TEXT,
+  extra TEXT,
+  name TEXT,
+  grouping TEXT,
+  fav TEXT
+);
+CREATE VIEW upm AS SELECT
+  "name" AS "account name",
+  "username" AS "user id",
+  "password",
+  "url",
+  "extra" AS "notes"
+FROM lastpass
+WHERE grouping != 'Secure Notes';
+EOF
+
 sqlite3 -csv $db ".import '$lastpasscsv' lastpass"
 sqlite3 $db 'SELECT * FROM upm'
 
